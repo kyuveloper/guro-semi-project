f1b1f5a1cc877d6b194a49946d437b928bcf671b
kyuveloper <jinkyuk127@gmail.com>
2023-12-15 18:28:38 +0900

    first commit

---
+++ src/main/java/com/gurosemiproject/GuroSemiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.gurosemiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuroSemiProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuroSemiProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gurosemiproject/GuroSemiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gurosemiproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuroSemiProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
