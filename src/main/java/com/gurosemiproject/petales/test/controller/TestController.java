fcaac7d2d147605a91322d9fca4035553a25ccde
hiimparkjiho <jiho2689@gmail.com>
2023-12-18 10:44:57 +0900

    setting

---
+++ src/main/java/com/gurosemiproject/petales/test/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.gurosemiproject.petales.test.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/test")
+public class TestController {
+    @RequestMapping("/test")
+    public String testCon(){
+        return "test/testView";
+    }
+}
