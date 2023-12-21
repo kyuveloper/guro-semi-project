d52c0e90c1c5ad6e4669a5eb6a15cf2c0a8be9da
hiimparkjiho <jiho2689@gmail.com>
2023-12-21 16:59:32 +0900

    페이징 처리

---
+++ src/main/java/com/gurosemiproject/petales/company/model/CompanyDAO.java
@@ -0,0 +1,21 @@
+package com.gurosemiproject.petales.company.model;
+
+import com.gurosemiproject.petales.company.dto.CompanyDTO;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface CompanyDAO {
+    // 전체 건물 조회 페이징
+    List<CompanyDTO> selectAllCompany(@Param("pageStart") int pageStart, @Param("pageLimit") int pageLimit);
+
+    // 지역별 건물 조회
+    List<CompanyDTO> selectAllCompanyByCtprvn(@Param("pageStart") int pageStart, @Param("pageLimit") int pageLimit, @Param("companyCtprvn") String companyCtprvn);
+    // 전체 시설의 수
+    int countCompany();
+
+    // 지역별 시설의 수
+    int countCompanyByCtprvn(@Param("companyCtprvn") String companyCtprvn);
+}
